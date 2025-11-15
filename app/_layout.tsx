eae22b9c26dc6e0fd7aac43d291c548adac52333
Adesina Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-11-15 19:29:32 +0100

    Set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+// Prevent splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync(); // Hide splash when ready
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    // Still loading → keep splash screen visible
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import "./global.css"
-import { Text, View } from "react-native";
- 
-export default function Index() {
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "./global.css";
+
+const Home = ()=> {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind css!
+        Welcome to Nativewind
       </Text>
-    </View>
+      <Text className="text-[20px] text-yellow-400  ">Hello world</Text>
+
+
+    </SafeAreaView>
   );
 }
+
+export default Home;
